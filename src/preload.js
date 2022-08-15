fb146f6ae098bb31e752731366d1621c18abfed0
Andriy Bohdan <Andriy Bohdan>
2022-08-15 17:34:43 +0200

    feat(preload): handel/send api

--- src/preload.js
+++ src/preload.js
@@ -1,5 +1,8 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
-contextBridge.exposeInMainWorld("darkMode", {
+contextBridge.exposeInMainWorld("api", {
+  send: (channel, data) => ipcRenderer.send(channel, data),
+  handle: (channel, callable, event, data) =>
+    ipcRenderer.on(channel, callable(event, data)),
   toggle: () => ipcRenderer.invoke("dark-mode:toggle"),
 });
