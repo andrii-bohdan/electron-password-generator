d6bdfdeb6ee62f40b1be35f12d943dd88a857b7c
Andriy Bohdan <Andriy Bohdan>
2022-08-15 17:35:10 +0200

    feat(main)!: backend logic. generate pass ...

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
 const { app, BrowserWindow, nativeTheme, ipcMain } = require("electron");
 const path = require("path");
 
+let win = null;
+
 function createWindow() {
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
+    resizable: false,
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false,
       preload: path.join(__dirname, "./src/preload.js"),
     },
   });
 
   win.webContents.openDevTools();
-
   win.loadFile("./index.html");
 
-  ipcMain.handle("dark-mode:toggle", () => {
+  ipcMain.handle("dark-mode:toggle", async () => {
     if (nativeTheme.shouldUseDarkColors) {
       nativeTheme.themeSource = "light";
     } else {
@@ -27,6 +28,7 @@ function createWindow() {
 }
 
 app.whenReady().then(() => {
+  nativeTheme.themeSource = "light";
   createWindow();
 
   app.on("activate", () => {
@@ -41,3 +43,39 @@ app.on("window-all-closed", () => {
     app.quit();
   }
 });
+
+ipcMain.on("generate-password", async (event, password) => {
+  const minLength = password.length > 12 ? password.length : 12;
+  const randomPassword = strongPasswordGenerator(password, minLength);
+  win.webContents.send("generate-password", randomPassword);
+});
+
+function strongPasswordGenerator(password, salt = 8) {
+  let signs = ["#", "%", "?", "!", "/", "", "#"];
+  let numbers;
+  let capital;
+  let small;
+
+  if (isNumber(password)) {
+    capital = ["A", "B", "C", "D", "E", "F", "G"];
+    small = ["a", "b", "c", "d", "e", "f", "g"];
+    numbers = [...password.split("")];
+  } else {
+    capital = password.toUpperCase();
+    small = password.toLowerCase();
+    numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
+  }
+
+  const mixDigits = [...signs, ...numbers, ...capital, ...small];
+  let strongPass = [];
+
+  for (let i = 0; i < mixDigits.length; i++) {
+    let hash = Math.floor(Math.random() * mixDigits.length);
+    strongPass.push(mixDigits[hash]);
+  }
+  return strongPass.join("").trim().slice(0, salt);
+}
+
+function isNumber(val) {
+  return /^\d+$/.test(val);
+}
