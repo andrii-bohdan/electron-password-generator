688fadeee30fb230d87af65991a47e81cd5675c6
Andriy Bohdan <Andriy Bohdan>
2022-08-19 18:46:57 +0200

    chore(debug): turn of debug

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function createWindow() {
     },
   });
 
-  win.webContents.openDevTools();
+  // win.webContents.openDevTools();
   win.loadFile("./index.html");
 
   ipcMain.handle("dark-mode:toggle", async () => {
